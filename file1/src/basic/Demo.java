1e0f2013ae602394d5256b6e8238cf4e50eea601
DELL <DELL@DESKTOP-AUT8MOL>
2024-10-24 21:26:17 +0530

    Second commit

---
+++ file1/src/basic/Demo.java
@@ -0,0 +1,28 @@
+package basic;
+
+class DefaultValuesChecking {
+	int i;
+	byte b;
+	float f;
+	double d;
+	char c1;
+	boolean b1;
+	String strvalue;
+	int a[];
+}
+public class Demo{
+
+	public static void main(String[] args) {
+		DefaultValuesChecking d1 = new DefaultValuesChecking();
+		System.out.println(d1.i);
+		System.out.println(d1.b);
+		System.out.println(d1.f);
+		System.out.println(d1.d);
+		System.out.println(d1.c1);
+		System.out.println(d1.b1);
+		System.out.println(d1.strvalue);
+		System.out.println(d1.a);
+
+	}
+
+}
