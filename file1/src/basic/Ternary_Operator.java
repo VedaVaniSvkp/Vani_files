a7cf9e0a8eeb07ec16d56ea7357c80d1e64a1fbf
DELL <DELL@DESKTOP-AUT8MOL>
2024-10-30 20:30:03 +0530

    Fifth commit

---
+++ file1/src/basic/Ternary_Operator.java
@@ -0,0 +1,11 @@
+package basic;
+
+public class Ternary_Operator {
+
+	public static void main(String[] args) {
+		int a = 20, b = 2;
+		int max = (a>b)?a:b;
+		System.out.println("max value is: "+max);
+	}
+
+}
