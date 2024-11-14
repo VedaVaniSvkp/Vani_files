26c80717f60a3614a80b70c7fd75421f7145837c
DELL <DELL@DESKTOP-AUT8MOL>
2024-11-14 19:55:09 +0530

    Third Commit

---
+++ file1/src/org/tnsif/capgemini/c2tc/oops/SuperMethod.java
@@ -0,0 +1,36 @@
+package org.tnsif.capgemini.c2tc.oops;
+
+class Animal5
+{
+	void makeSound()
+	{
+		System.out.println("Animal makes a sound");
+	}
+}
+
+class Dog5 extends Animal5
+{
+	void makeSound()
+	{
+		super.makeSound();
+	   System.out.println("Dog barks");
+	}
+}
+
+class Cat5 extends Animal5
+{
+	void makeSound()
+	{
+		super.makeSound();
+		System.out.println("Cat meows");
+	}
+}
+public class SuperMethod {
+
+	public static void main(String[] args) {
+		Dog5 dog = new Dog5();
+		dog.makeSound();
+
+	}
+
+}
