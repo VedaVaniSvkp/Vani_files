66aef9212039cf7d829f949d1dd6005830403b22
DELL <DELL@DESKTOP-AUT8MOL>
2024-11-23 22:51:40 +0530

    First Commit

---
+++ file1/src/org/tnsif/capgemini/c2tc/constructor/BeforeConstructor.java
@@ -0,0 +1,16 @@
+package org.tnsif.capgemini.c2tc.constructor;
+
+class MyClass{
+	String color;
+	int cost;
+	String brand;
+}
+
+public class BeforeConstructor {
+
+	public static void main(String[] args) {
+		MyClass obj = new MyClass();
+		System.out.println(obj.color+" "+obj.cost+" "+obj.brand);
+	}
+
+}
