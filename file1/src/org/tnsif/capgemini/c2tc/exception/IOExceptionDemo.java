8edf2357b1ebbcc31abab58f83d1e80baae71e12
DELL <DELL@DESKTOP-AUT8MOL>
2024-12-03 19:42:21 +0530

    First Commit

---
+++ file1/src/org/tnsif/capgemini/c2tc/exception/IOExceptionDemo.java
@@ -0,0 +1,22 @@
+package org.tnsif.capgemini.c2tc.exception;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+public class IOExceptionDemo {
+
+	public static void main(String[] args) throws Exception{
+		try {
+			BufferedReader br = new BufferedReader(new FileReader("C:\\vani\\C# programs"));
+			String data = " ";
+			while((data=br.readLine())!=null) {
+				System.out.println(data);
+			}
+			br.close();
+		}
+		catch(IOException e)
+		{
+			System.out.println(e);
+		}
+	}
+}
