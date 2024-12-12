729367ee4b295cc6412a3ed6e17634f12b065604
DELL <DELL@DESKTOP-AUT8MOL>
2024-12-12 18:26:22 +0530

    First Commit

---
+++ file1/src/org/tnsif/capgemini/c2tc/generics/GenericClass.java
@@ -0,0 +1,38 @@
+package org.tnsif.capgemini.c2tc.generics;
+
+class DataContainer<T>
+{
+	private T data;
+	
+public DataContainer(T data)
+{
+	this.data = data;
+}
+public T getData()
+{
+	return data;
+}
+public void displayData()
+{
+	System.out.println("Data type "+data.getClass().getSimpleName());
+	System.out.println("Data Value "+data);
+}
+	
+}
+public class GenericClass {
+
+	public static void main(String[] args) {
+		DataContainer<Integer> integerContainer = new DataContainer<>(123);
+		integerContainer.displayData();
+		
+		DataContainer<String> integerContainer1 = new DataContainer<>("Java Generics");
+		integerContainer1.displayData();
+		
+		DataContainer<Double> integerContainer2 = new DataContainer<>(26.9);
+		integerContainer2.displayData();
+
+
+
+	}
+
+}
