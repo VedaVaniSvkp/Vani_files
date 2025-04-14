d559f8b1ee1464522b908b3b5aa55f4dfe12a4e7
DELL <DELL@DESKTOP-AUT8MOL>
2025-04-14 17:40:56 +0530

    Second Commit

--- file1/src/org/tnsif/capgemini/c2tc/practicalquestions/Person.java
+++ file1/src/org/tnsif/capgemini/c2tc/practicalquestions/Person.java
@@ -1,9 +1,21 @@
 package org.tnsif.capgemini.c2tc.practicalquestions;
 
 public class Person {
+	int age=20;
+	public int vote() {
+		if(age>=18) {
+			System.out.println("vote");
+			return 0;
+		}
+		else {
+			System.out.println("Not vote");
+			return -1;
+		}
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Person per = new Person();
+		per.vote();
 
 	}
 
